7617e023f2152e19666bc66b24f6b2198d554979
Ryan Chen <ryanjchen@yahoo.com>
2017-10-22 05:44:20 -0700

    ok all good now

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var twilio_auth = process.env.twilio_auth;
 const client = require('twilio')(twilio_sid, twilio_auth);
 
 client.calls.create({
-  url: './voice.xml',
+  url: 'https://handler.twilio.com/twiml/EHb4eebdcb8f5f7a35efb023551586619e',
   to: '+14087755735',
   from: twilio_number,
 })
