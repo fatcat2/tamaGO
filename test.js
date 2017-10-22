2c1e5587d82805733f6377b32197659555948b26
Ryan Chen <ryanjchen@yahoo.com>
2017-10-22 05:37:03 -0700

    whoops

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const twilio_number = '+17656370247';
 var twilio_sid = process.env.twilio_sid;
 var twilio_auth = process.env.twilio_auth;
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(twilio_sid, twilio_auth);
 
 client.calls.create({
   url: 'http://demo.twilio.com/docs/voice.xml',
